1177bdb3278d36da64de36ae6136bfbbc0c547a1
Alex Handley <alex@seriousfox.co.uk>
2015-02-14 15:33:48 +0000

    adds the ability to have default params
    default_args can be overriden to create the args when running commands

--- spec/analysers/base_analyser_spec.rb
+++ spec/analysers/base_analyser_spec.rb
@@ -10,7 +10,23 @@ describe BaseAnalyser do
 			@klass = Class.new(BaseAnalyser)
 
 			analyser = @klass.new(params)
-			expect(analyser).to receive(:system).with(params[:command])
+			expect(analyser).to receive(:system).with("#{params[:command]} ")
+			analyser.run
+		end
+
+		it "should allow args to be injected into command str" do
+			params = {
+				command: "madeup spec"
+			}
+
+			@klass = Class.new BaseAnalyser do
+				def default_args
+					["beam_me_up"]
+				end
+			end
+
+			analyser = @klass.new(params)
+			expect(analyser).to receive(:system).with("#{params[:command]} beam_me_up")
 			analyser.run
 		end
 	end
