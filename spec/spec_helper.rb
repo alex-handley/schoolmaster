02c1bb0cf9170e443790b810ad3aabb227557645
Alex Handley <alex@seriousfox.co.uk>
2015-02-20 16:18:09 +0000

    changes file to double quotes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require 'rubygems'
-require 'bundler/setup'
+require "rubygems"
+require "bundler/setup"
 
-require_relative '../lib/schoolmaster'
+require_relative "../lib/schoolmaster"
 
 RSpec.configure do |config|
   config.color = true
-	config.order = 'random'
+	config.order = "random"
 end
 
