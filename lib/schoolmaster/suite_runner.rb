395c3c83f882423d532c1459ad56db6c52fc129d
Alex Handley <alex@seriousfox.co.uk>
2015-02-20 17:26:31 +0000

    adds custom configuration to analysers

--- lib/schoolmaster/suite_runner.rb
+++ lib/schoolmaster/suite_runner.rb
@@ -1,10 +1,19 @@
 module Schoolmaster
 	class SuiteRunner
 		def self.run
-			Schoolmaster::CaneAnalyser.new(args: Schoolmaster.configuration.cane_custom_args).run
-			Schoolmaster::BestPracticesAnalyser.new.run if defined?(Rails)
-			Schoolmaster::BrakemanAnalyser.new.run
-			Schoolmaster::TestAnalyser.new.run
+			Schoolmaster::CaneAnalyser.new(
+																	args: Schoolmaster.configuration.cane_custom_args).run
+
+			if defined?(Rails)
+				Schoolmaster::BestPracticesAnalyser.new(
+												args: Schoolmaster.configuration.best_practices_custom_args).run
+			end
+
+			Schoolmaster::BrakemanAnalyser.new(
+															args: Schoolmaster.configuration.brakeman_custom_args).run
+
+			Schoolmaster::TestAnalyser.new(
+																	args: Schoolmaster.configuration.test_custom_args).run
 		end
 	end
 end
